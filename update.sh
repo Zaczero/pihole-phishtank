63e8476bd7594759f88e895f0be517e7399b4e9b
Kamil Monicz <kamil@monicz.pl>
2023-09-16 14:24:08 +0200

    Retry include JSON validation

--- update.sh
+++ update.sh
@@ -1,29 +1,45 @@
 #!/usr/bin/env bash
+# shellcheck disable=SC2181
 set -e
 
 # cwd to script directory
 cd "$(dirname "$0")"
 
-echo "Fetching latest data from phishtank..."
 fetch_data() {
   for _ in $(seq 1 10); do
+    # fetch the data
     data=$(curl -sL https://data.phishtank.com/data/online-valid.json)
-    if [[ $? -eq 0 && -n "$data" ]]; then
-      return 0
+
+    # check if curl was successful
+    if [[ $? -ne 0 ]]; then
+      echo "Curl failed. Retrying..."
+      sleep 60
+      continue
     fi
-    sleep 60
+
+    # check if data is non-empty
+    if [[ -z "$data" ]]; then
+      echo "Received empty response. Retrying..."
+      sleep 60
+      continue
+    fi
+
+    # validate JSON
+    if ! echo "$data" | jq empty; then
+      echo "Received invalid JSON. Retrying..."
+      sleep 60
+      continue
+    fi
+
+    return 0
   done
+
   return 1
 }
 
+echo "Fetching latest data from phishtank..."
 if ! fetch_data; then
-  echo "Failed to fetch data after multiple attempts"
-  exit 1
-fi
-
-# check if the data is a valid JSON
-if ! echo "$data" | jq empty; then
-  echo "Received invalid JSON data"
+  echo "Failed to fetch data after multiple attempts."
   exit 1
 fi
 
